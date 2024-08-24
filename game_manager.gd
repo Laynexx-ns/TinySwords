744f74ad38023a10dd62acfbebdb0ffbbf40380c
Laynexx-ns <argo150808@gmail.com>
2024-08-24 23:26:48 +0300

    camera smoothing

--- game_manager.gd
+++ game_manager.gd
@@ -2,6 +2,11 @@ extends Node
 
 
 var default_arrow = load("res://All/Assets/Effects/01.png")
+@onready var knight_camera: Camera2D = $"../SceneObjs/Characters/Knight/KnightCamera"
+@onready var archer_camera: Camera2D = $"../Archer/ArcherCamera"
+
+
+var knight : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +17,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if (Input.is_action_just_pressed("change_camera")):
+		if (knight):
+			knight_camera.enabled = false
+			archer_camera.enabled = true
+			knight = false
+		else:
+			knight_camera.enabled = true
+			archer_camera.enabled = false
+			knight = true
